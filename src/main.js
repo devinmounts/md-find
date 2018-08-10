2f9f441023788845b34d2bd815196f08643f99e2
Devin Mounts <devinmounts@gmail.com>
2018-08-10 13:56:29 -0700

    create docObject for each doctor that matches search criteria.

--- src/main.js
+++ src/main.js
@@ -14,8 +14,9 @@ $(document).ready(() => {
     for(let i=0; i<body.data.length; i++) {
       const condition = body.data[i].name;
       $('#conditionsDropdown').append(`<option>${condition}</option>`);
-    }}, function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    }
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
 
   $('#conditions-form').submit(function(event){
@@ -24,40 +25,51 @@ $(document).ready(() => {
 
     const promiseQuery = search.getDoctors(query);
 
-    promiseQuery.then((response) => {
+    promiseQuery.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
-      console.log(body.data[0].practices[0].visit_address.city);
-      console.log(body.data[0].practices[0].website);
-      const docObject = {};
+      const docArray = [];
       for(let i=0; i<body.data.length; i++){
+        const docObject = {};
+        console.log(docObject);
         const firstName = body.data[i].profile.first_name;
         const lastName = body.data[i].profile.last_name;
         const title = body.data[i].profile.title;
+        docObject["firstName"] = firstName;
+        docObject["lastName"] = lastName;
+        docObject["title"] = title;
         for(let j=0; j<body.data[i].practices.length; j++){
           const inArea = body.data[i].practices[j].within_search_area;
-          const hasWebsite = body.data[i].practices[j].website
+          const hasWebsite = body.data[i].practices[j].website;
           if( inArea === true ){
-            const accpeptsNewPatient = body.data[i].practices[j].accepts_new_patients;
+            const acceptsNewPatient = body.data[i].practices[j].accepts_new_patients;
             const city = body.data[i].practices[j].visit_address.city;
             const state = body.data[i].practices[j].visit_address.state;
             const street = body.data[i].practices[j].visit_address.street;
-            const zip = body.data[i].practices[j].visit_address.street;
-            if (hasWebsite != undefined){
-              const website = body.data[i].practices[j].website
+            const zip = body.data[i].practices[j].visit_address.zip;
+            docObject["acceptsNewPatient"] = acceptsNewPatient;
+            docObject["city"] = city;
+            docObject["state"] = state;
+            docObject["street"] = street;
+            docObject["zip"] = zip;
+          }
+          if(hasWebsite !== undefined){
+            const website = body.data[i].practices[j].website;
+            docObject["website"] = website;
+          }
+          for(let k=0; k<body.data[i].practices[j].phones.length; k++){
+            const isLandLine = body.data[i].practices[j].phones[k].type;
+            if( isLandLine === "landline"){
+              const phone = body.data[i].practices[j].phones[k].number;
+              docObject["phone"] = phone;
             }
-            for(let k=0; k<body.data[i].practices[j].phones.length; k++){
-              const isLandLine = body.data[i].practices[j].phones[k].type
-              if( isLandLine === "landline")
-                const phone = body.data[i].practices[j].phones[k].number;
-
-            }
-
-
           }
         }
+        console.log(docObject);
+        docArray.push(docObject);
       }
-
+      console.log(docArray);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
