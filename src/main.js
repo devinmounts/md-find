b473cc026514f4df28e1421141224bb149129c09
Devin Mounts <devinmounts@gmail.com>
2018-08-10 12:05:58 -0700

    nest for loop to gather address info and nest for loop to gather phone.

--- src/main.js
+++ src/main.js
@@ -28,15 +28,31 @@ $(document).ready(() => {
       const body = JSON.parse(response);
       console.log(body);
       console.log(body.data[0].practices[0].visit_address.city);
-
+      console.log(body.data[0].practices[0].phones[0].type);
+      const docObject = {};
       for(let i=0; i<body.data.length; i++){
         const firstName = body.data[i].profile.first_name;
         const lastName = body.data[i].profile.last_name;
         const title = body.data[i].profile.title;
-        const address = body.data[i]
+        for(let j=0; j<body.data[i].practices.length; j++){
+          const inArea = body.data[i].practices[j].within_search_area;
+          if( inArea === true ){
+            const accpeptsNewPatient = body.data[i].practices[j].accepts_new_patients;
+            const city = body.data[i].practices[j].visit_address.city;
+            const state = body.data[i].practices[j].visit_address.state;
+            const street = body.data[i].practices[j].visit_address.street;
+            const zip = body.data[i].practices[j].visit_address.street;
+            for(let k=0; k<body.data[i].practices[j].phones.length; k++){
+              const isLandLine = body.data[i].practices[j].phones[k].type
+            }
+              if( isLandLine === "landline")
+            const phone = body.data[i].practices[j].phones.street;
+
+      
+          }
+        }
       }
-      const firstName = body.
-      if ()
+
     });
   });
 });
