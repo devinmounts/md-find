3bbca061f4ec7188899fd716b96cdfde03fca921
Devin Mounts <devinmounts@gmail.com>
2018-08-10 11:37:39 -0700

    create for loop to gather profile info.

--- src/main.js
+++ src/main.js
@@ -17,16 +17,26 @@ $(document).ready(() => {
     }}, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
-  //
-  // $('#conditions-form').submit(function(event){
-  //   event.preventDefault();
-  //   const query = $('#conditionsDropdown').find(':selected').text();
-  //
-  //   const promiseQuery = search.getDoctors(query);
-  //
-  //   promiseQuery.then((response) => {
-  //     const body = JSON.parse(response);
-  //     console.log(body);
-  //   });
-  // });
+
+  $('#conditions-form').submit(function(event){
+    event.preventDefault();
+    const query = $('#conditionsDropdown').find(':selected').text();
+
+    const promiseQuery = search.getDoctors(query);
+
+    promiseQuery.then((response) => {
+      const body = JSON.parse(response);
+      console.log(body);
+      console.log(body.data[0].practices[0].visit_address.city);
+
+      for(let i=0; i<body.data.length; i++){
+        const firstName = body.data[i].profile.first_name;
+        const lastName = body.data[i].profile.last_name;
+        const title = body.data[i].profile.title;
+        const address = body.data[i]
+      }
+      const firstName = body.
+      if ()
+    });
+  });
 });
